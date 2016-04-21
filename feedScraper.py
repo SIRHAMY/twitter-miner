ea286888dceb69a0ea730eef52616ca16bcdf6dd
Hamilton Greene <hamilton.greene@gmail.com>
2016-04-20 23:04:26 -0400

    feedScraper moved to Python and pulls in feeds

--- feedScraper.py
+++ feedScraper.py
@@ -4,6 +4,17 @@ import time
 import timeit
 import tweetpony
 
+class JSONFetcher():
+	def __init__(self, fileName):
+		self.fileName = fileName + '.json'
+
+	def fetch(self):
+		print("fetching...")
+
+		with open(self.fileName, 'rb') as f:
+			for line in f:
+				yield json.loads(line)
+
 class FeedScraper():
 
 	def __init__(self):
@@ -15,138 +26,156 @@ class FeedScraper():
 		user = self.twitAPI.user
 		print ("Hello " + user.screen_name)
 
+		self.stats = {
+			'tweetsProcessed' = 0,
+			'feedsProcessed' = 0,
+		}
+
 	#Perform ze mining
 	def mine(self):
 
+		#Ensure followers file exists
 		try:
 			print("Searching for raw followers...")
-			followers = 
+			self.followers = JSONFetcher(env.TWITTER_HANDLE + "Followers")
+			print("Raw followers found.")
+		except IOError as err:
+			print("No raw followers found...")
+			print("Disbanding expedition.")
+			sys.exit("No follower file found for " + env.TWITTER_HANDLE)
+		except Exception as err:
+			print("ERROR: Couldn't open file")
+			print(str(err))
 
-		if( ( self.mongoUserIDs.count() == 0 ) ):
-			print("ABORTED OPERATION: Couldn't locate UserID list")
-			print("--Suggested actions: Ensure mongodb " + env.TWITTER_HANDLE + ".followers exists")
-			print("--Suggested actions: Ensure mongodb TWITTER_HANDLE.followers is populated")
-			return
-		else:
-			print "Found deposit of user IDs..."
-
-			#Have followers list, need to check if we have started already
-			lastMiningIndex = -1
-			lastTwitterID = -1
-			if( self.mongoUserFeeds.count() > 0 ):
-				print("Existing mining operation found")
-
-				#Attempt to figure out where mining stopped
-				print("Attempting to retrace steps...")
-				try:
-					cursor = self.mongoUserFeeds.find().sort( [ ["_id", -1] ] ).limit(1)
-					lastMiningIndex = cursor[0]['userPageID']
-					lastTwitterID = cursor[0]['twitterID']
-				except Exception as e:
-					print("Error retracing steps")
-					print str(e)
-					return
-
-			else:
-				print("No existing mining data found")
-				print("Initializing new expedition...")
-
-			self.mineProcess(lastMiningIndex, lastTwitterID)
-
-	def mineProcess(self, lastMiningIndex=-1, lastTwitterID=-1):
-		print("Feeding earth benders...")
+		lastMiningIndex = -1
+		lastTwitterID = -1
 
 		try:
-			mineCursor = None
+			print("Searching for existing feed expedition...")
+			
+			testFile = open(env.TWITTER_HANDLE + "Feeds.json", "rb")
+			print("Expedition found.")
+			testFile.close()
 
-			if(lastMiningIndex == -1):
-				mineCursor = self.mongoUserIDs.find().sort( [ ["_id", 1] ] )
-			else:
-				mineCursor = self.mongoUserIDs.find( {'_id': {'$gte': lastMiningIndex } }).sort( [ ["_id", 1] ] )
-
-			newProcess = (lastTwitterID == -1)
-
-			#HAMYChange
-			print("MineProcess cursor length: ")
-			print mineCursor.count()
-			print mineCursor[0]['_id']
-			print lastMiningIndex
-
-			for idStash in mineCursor:
-				print "Iterate cursor"
-
-				twitterIDs = idStash['followers']
-				for twitterID in twitterIDs:
-					print("Looking for twitterID")
-
-					if(not newProcess):
-						print(int(twitterID))
-						#print( int(lastTwitterID) )
-						if( int(twitterID) == int(lastTwitterID) ): 
-							print("Found last Twitter ID")
-							newProcess = True
-					else:
-						self.fetchFeed(int(twitterID), idStash['_id'])
-
-			print("Reached end of mining cursor")
-
+			self.feeds = JSONFetcher(env.TWITTER_HANDLE + "Feeds")
+		except IOError as err:
+			print("No expedition found...")
+			print("Starting new expedition.")
 		except Exception as err:
-			print "ERROR: Issue in mine process"
-			print str(err)
+			print("ERROR: Problem finding existing feed mining expedition")
+			print(str(err))
+		else:
+			line = None
+			for line in self.feeds.fetch():
+				pass
+			if(line):
+				lastMiningIndex = line['nextCursor']
+				lastTwitterID = line['twitterID']
 
+		self.mineProcess(lastMiningIndex, lastTwitterID)
 
+	def mineProcess(self, lastMiningIndex=-1, lastTwitterID=-1):
+		print("Prepping earth benders...")
 
-	def fetchFeed(self, twitterID, userPageID):
+		existingOperation = False 
+		if(lastMiningIndex != -1):
+			existingOperation = True 
+
+		try:
+			#TODO: Does this need to change?
+			self.writeFeeds = open(env.TWITTER_HANDLE + "Feeds.json", 'ab')
+
+			for record in self.followers.fetch():
+				if(existingOperation and record['nextCursor'] != lastMiningIndex):
+					pass
+				else:
+					for follower in record['twitterIDs']:
+						if(existingOperation):
+							if(follower != lastTwitterID):
+								pass
+							else:
+								print("Found previous mining endpoint.")
+								existingOperation = False
+						else:
+							newFeed = self.mineFeed(follower, record['nextCursor'])
+		except Exception as err:
+			print("ERROR: Mining disaster")
+			print(str(err))
+		else:
+			print("OPERATION ENDED: Mining complete")
+			self.writeFeeds.close()
+
+	def mineFeed(self, twitterID, srcPageID):
 		print("Fetching feed...")
 
 		try:
 			userFeed = self.twitAPI.user_timeline(user_id=str(int(twitterID)), exclude_replies="true", include_rts="false" )
-
-			self.insertFeed(twitterID, userFeed, userPageID)
+			self.storeFeed(twitterID, userFeed, srcPageID)
 		except Exception as err:
 			if('88' in str(err)):
 				print("Mining Suspended: Reached Twitter rate limit")
 				print("Waiting 3 minutes then retrying...")
 				time.sleep(180)
-				self.fetchFeed( twitterID, userPageID)
+				self.fetchFeed( twitterID, srcPageID)
 			else:
 				print("ERROR: Something broke when fetching the feed")
 				print str(err)
 		else:
 			print("Fetch sucessful.")
 
-	def insertFeed(self, twitterID, userFeed, userPageID):
+	def storeFeed(self, twitterID, userFeed, srcPageID):
 		print("Inserting feed...")
+		print(userFeed)
 
 		try:
-			if(userFeed[1] is not None):
-				self.mongoUserFeeds.insert_one(
-					{
-						"twitterID": twitterID,
-						"userPageID": userPageID,
-						"userInfo": userFeed[0],
-						"feed": userFeed[1:]
-					})
+			print("HAMY: userfeed[0]")
+			print userFeed[0]
+			print("HAMY: userfeed[1]")
+			print userFeed[1]
+
+			if(userFeed[0] is not None):
+				tweetList = []
+				for tweet in userFeed:
+					tweetList.append(tweet['text'])
+
+				toWrite = {"twitterID": twitterID, "nextCursor": srcPageID, 
+							"feed": tweetList}
+				jsonOut = json.dumps(toWrite, separators=(',',':'))
+
+				self.writeFeeds.write(jsonOut + '\n')
 
 			#HAMYChange
 			#print("userID: " + str(userFeed[0]['id']) )
 			#print("userPageID: " + str(userPageID) )
 		except Exception as err:
 			print "ERROR: Problem with insertion"
+			print str(err)
 			error = str(err)
 			if("list index out of range" in error):
 				print("User had no tweets fitting the search criteria")
 		else:
 			print("Successfully inserted: " + str(userFeed[0]['id']) )
 
+	def printStats(self):
+		print( "Tweets processed: " + str(stats.tweetsProcessed) )
+		print( "Feeds Processed: " + str(stats.feedsProcessed) )
+
 
 
 
 #Run the class holmes				
-
 if __name__ == '__main__':
+
+	timeStart = timeit.default_timer()
+	print("Operation started at: " + repr(timeStart))
+
 	scraper = FeedScraper()
 	scraper.mine()
+
+	timeEnd = timeit.default_timer()
+	print("Elapsed Time: " + repr(timeEnd - timeStart) + "s")
+
+	scraper.printStats()
 
 
 	
